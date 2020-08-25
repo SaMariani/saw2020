4f4c2c8095af777f48d84c584e3a9f390ba2cebb
prosciutto44 <samuele.mariani.01@gmail.com>
2020-08-26 00:28:31 +0200

    messo apposto foto in modalità list

--- cestino/producs.php
+++ cestino/producs.php
@@ -138,7 +138,7 @@ if(!isset($_SESSION['myusersaw']))
                 template+=
                     "<div class='item  col-xs-4 col-lg-4'>"+
                     "<div class='thumbnail'>"+
-                    "<img class='group list-group-image' src='http://placehold.it/400x250/000/fff' alt='' />"+
+                    "<img class='group list-group-image' style='max-height: 250px' src='http://placehold.it/400x250/000/fff' alt='' />"+
                     "<div class='caption'>"+
                     "<h4 class='group inner list-group-item-heading'>"+
                     myObj[x].nomeprodotto + "</h4>"+

ff80f77e62afb14013dd3b59337d8ad1842eddb4
prosciutto44 <samuele.mariani.01@gmail.com>
2020-08-26 12:42:07 +0200

    sistemato conflitto id per css

--- db/mysql_credentials.php
+++ db/mysql_credentials.php
@@ -1,11 +1,6 @@
 <?php
-/*
+
 $mysql_host = "localhost";
 $mysql_user = "S4538910";
 $mysql_pass = "SamuMassi897";
 $mysql_db = "S4538910";
-*/
-$mysql_host = "localhost";
-$mysql_user = "root";
-$mysql_pass = "";
-$mysql_db = "saw";
